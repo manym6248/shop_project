1bedf8f473bf4be6718952f29ff6e94c21476f82
unknown <manym6248@gmail.com>
2021-05-18 14:06:33 +0430

    deleted a:heloo

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,22 +1,17 @@
 <template>
-  <v-container fluid class="pa-0 ">
+  <v-container fluid class="pa-0">
     <div class="media-header mb-8">
       <h3>وبلاگ</h3>
-      <v-breadcrumbs large  :items="items"
-      divider="/"></v-breadcrumbs>
+      <v-breadcrumbs large :items="items" divider="/"></v-breadcrumbs>
     </div>
 
-    <v-container class="pt-4 pb-3 px-0 mb-4">
-      
-    </v-container>
+    <v-container class="pt-4 pb-3 px-0 mb-4"> </v-container>
   </v-container>
-    
 </template>
 <script>
 export default {
   data() {
     return {
-       
       items: [
         {
           text: "صفحه اصلی",
@@ -26,7 +21,6 @@ export default {
         {
           text: "وبلاگ",
           disabled: true,
-          
         },
       ],
     };
